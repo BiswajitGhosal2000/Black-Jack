a9e4db40c405d3dceb7bec114b9510a0d533d8c3
BISWAJIT GHOSAL <72649700+BiswajitGhosal2000@users.noreply.github.com>
2022-12-30 19:26:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,42 @@
-let firstCard = Math.floor(Math.random()*11 + 1);
-let secondCard = Math.floor(Math.random()*11 + 1);
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let cards = [];
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.querySelector("#sum-el");
 let cardEl = document.getElementById("card-el");
-// let StartGame = document.querySelector("#startGame");
 
 function startGame() {
+    let firstCard = Math.floor(Math.random()*11 + 1);
+    let secondCard = Math.floor(Math.random()*11 + 1);
+    cards.push(firstCard);
+    cards.push(secondCard);
     renderGame();
+    document.querySelector("#startGame").style.display = "none";
 }
 
 function renderGame() {
+    let sum = 0;
+    for(let i = 0; i < cards.length; i++){
+        sum += cards[i]
+    }
     if (sum <= 20) {
         message = "Do you want to draw a new card?";
+        isAlive = true;
+        document.querySelector("#newCard").style.display = "block";
 
     } else if (sum == 21) {
         message = "Congrats!you've got BlackJack";
         hasBlackJack = true;
+        document.querySelector("#newCard").style.display = "none";
         window.setTimeout(function(){
             window.location.reload();
         },3000);
     } else {
         message = "You're out of the game.\n please wait a second to play again ";
         isAlive = false;
+        document.querySelector("#newCard").style.display = "none";
         window.setTimeout(function(){
             window.location.reload();
         },3000);
@@ -44,9 +53,10 @@ function renderGame() {
 }
 
 function newCard() {
+    if (isAlive === true && hasBlackJack === false){
     console.log("drawing  a new card from the deck!!")
     let newCard = Math.floor(Math.random()*11 + 1);
     cards.push(newCard);
-    sum += newCard;
     renderGame();
+    }
 }
